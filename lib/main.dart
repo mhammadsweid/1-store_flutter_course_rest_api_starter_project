4099925399a58dfbbb122fbba985a7e1ce2f6e3c
kareemelbenni <82087428+mhammadsweid@users.noreply.github.com>
2023-01-11 12:35:29 +0200

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'consts/global_colors.dart';
 import 'screens/home_screen.dart';
@@ -13,47 +14,57 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter 3.0.4 ',
-      theme: ThemeData(
-        scaffoldBackgroundColor: lightScaffoldColor,
-        primaryColor: lightCardColor,
-        backgroundColor: lightBackgroundColor,
-        appBarTheme: AppBarTheme(
-          iconTheme: IconThemeData(
-            color: lightIconsColor,
-          ),
-          backgroundColor: lightScaffoldColor,
-          centerTitle: true,
-          titleTextStyle: TextStyle(
-              color: lightTextColor, fontSize: 22, fontWeight: FontWeight.bold),
-          elevation: 0,
-        ),
-        iconTheme: IconThemeData(
-          color: lightIconsColor,
-        ),
-
-        textSelectionTheme: const TextSelectionThemeData(
-          cursorColor: Colors.black,
-          selectionColor: Colors.blue,
-
-          // selectionHandleColor: Colors.blue,
-        ),
-
-        // textTheme: TextTheme()
-        // textTheme: Theme.of(context).textTheme.apply(
-        //       bodyColor: Colors.black,
-        //       displayColor: Colors.black,
-        //     ),
-        cardColor: lightCardColor,
-        brightness: Brightness.light,
-        colorScheme: ThemeData().colorScheme.copyWith(
-              secondary: lightIconsColor,
-              brightness: Brightness.light,
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return
+        MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter 3.0.4 ',
+          theme: ThemeData(
+            scaffoldBackgroundColor: lightScaffoldColor,
+            primaryColor: lightCardColor,
+            backgroundColor: lightBackgroundColor,
+            appBarTheme: AppBarTheme(
+              iconTheme: IconThemeData(
+                color: lightIconsColor,
+              ),
+              backgroundColor: lightScaffoldColor,
+              centerTitle: true,
+              titleTextStyle: TextStyle(
+                  color: lightTextColor,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
+              elevation: 0,
             ),
-      ),
-      home: const HomeScreen(),
+            iconTheme: IconThemeData(
+              color: lightIconsColor,
+            ),
+
+            textSelectionTheme: const TextSelectionThemeData(
+              cursorColor: Colors.black,
+              selectionColor: Colors.blue,
+
+              // selectionHandleColor: Colors.blue,
+            ),
+
+            // textTheme: TextTheme()
+            // textTheme: Theme.of(context).textTheme.apply(
+            //       bodyColor: Colors.black,
+            //       displayColor: Colors.black,
+            //     ),
+            cardColor: lightCardColor,
+            brightness: Brightness.light,
+            colorScheme: ThemeData().colorScheme.copyWith(
+                  secondary: lightIconsColor,
+                  brightness: Brightness.light,
+                ),
+          ),
+          home: const HomeScreen(),
+        );
+      },
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,14 +9,54 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+ late TextEditingController _textEditingController;
+  @override
+  void initState() {
+    _textEditingController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child: const Scaffold(
-        body: Center(child: Text("Welcome to this course")),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Home"),
+          leading: IconButton(
+            icon: const Icon(Icons.category),
+            // tooltip: 'Menu Icon',
+            onPressed: () {},
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.person),
+              // tooltip: 'Menu Icon',
+              onPressed: () {},
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(),
+          child: Column(
+            children: [
+              SizedBox(
+                height: ScreenUtil().setHeight(20),
+              ),
+              TextField(
+                     controller: _textEditingController,
+                  )
+            ],
+          ),
+        ),
       ),
     );
   }
